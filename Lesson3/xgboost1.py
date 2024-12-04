991c5b7f5627461fc55e7ed548f8c0f95be3da85
Haim Gilboa <56435506+haimnewclass@users.noreply.github.com>
2024-12-04 06:00:09 +0200

    Boost 2

--- Lesson3/xgboost1.py
+++ Lesson3/xgboost1.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# נתונים
+# נתונים 11
 data = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
 
